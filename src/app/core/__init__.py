491304c19d5ff347e67b98c1407389dc02ad1a1b
Artur Samarin <asamarin97@gmail.com>
2018-12-08 02:01:30 +0300

    Fix import problem

--- src/app/core/__init__.py
+++ src/app/core/__init__.py
@@ -1,6 +1,6 @@
 from .game import Game
 from .games_registry import GamesRegistry
-from app.core.language import Language, LanguageRegistry
+from .language import Language, LanguageRegistry
 from .game_result import GameOutcome
 from .rating_system import RatingSystem, EloRatingSystem
 
